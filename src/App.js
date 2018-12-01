9c57ee5374f9912bccc40414bbf9eb50f05a899d
Ron Krasniqi <ronkrasniqi_@hotmail.co.uk>
2018-12-01 12:10:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/vi';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}${DEFAULT_QUERY}`;
 
 const isSearched = searchTerm => item => 
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -27,12 +16,25 @@ const isSearched = searchTerm => item =>
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { list,
-                   searchTerm : '', 
+    this.state = { result: null,
+                   searchTerm : DEFAULT_QUERY, 
                   };
 
-    this.onDismiss = this.onDismiss.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({result});
+  }
+
+  componentDidMount() {
+    const {searchTerm} = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories({result}))
+    .catch(error => error);
   }
 
   onDismiss(id) {
