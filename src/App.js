0b42c8ee7083cddb839d2e0c5641c6ee89041ee6
Ron mac <ronkrasniqi_@hotmail.co.uk>
2019-01-21 22:29:07 +0000

    Pagination
    Added pagination to the hacker news app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
-const url = `${PATH_BASE}${PATH_SEARCH}${DEFAULT_QUERY}`;
+const url = `${PATH_BASE}${PATH_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
 
 class App extends Component {
   constructor(props) {
@@ -23,8 +26,8 @@ class App extends Component {
     this.onDismiss = this.onDismiss.bind(this);
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
     .catch(error => error);
@@ -37,12 +40,26 @@ class App extends Component {
 
   onSearchSubmit(event) {
     const {searchTerm} = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.fetchSearchTopStories(searchTerm); 
     event.preventDefault();
   }
 
   setSearchTopStories(result) {
-    this.setState({result});
+    const {hits, page} = result;
+
+    const oldHits = page !== 0
+      ? this.state.result.hits
+        : [];
+
+    const updatedHits = [
+        ...oldHits,
+        ...hits
+    ]
+
+    this.setState({
+      result: { hits: updatedHits, page }
+    })
+
   }
 
 
@@ -62,6 +79,7 @@ class App extends Component {
 
   render() {
     const {searchTerm, result} = this.state;
+    const page = (result && result.page) || 0;
     return (
       <div className="page">
       <div className="interactions">
@@ -80,6 +98,11 @@ class App extends Component {
         onDismiss={this.onDismiss}
         />
       }
+      <div className="interactions">
+      <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+          More
+      </Button>
+      </div>
       </div>
     );
   }
